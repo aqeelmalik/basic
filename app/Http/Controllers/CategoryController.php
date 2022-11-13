8a892f00c8bfea13851dc7769d643e3723c8e657
Aqeel Malik <aqeelmalik615@gmail.com>
2022-11-13 19:05:58 +0500

    FL-0010 Insert Data using Query Builder

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
@@ -27,17 +28,24 @@ class CategoryController extends Controller
             ]);
 
         //Insert data using Eloquent ORM method
-        Category::insert([
-            'category_name' => $request->category_name,
-            'user_id' => Auth::user()->id,
-            'created_at' => Carbon::now()
-        ]);
+//        Category::insert([
+//            'category_name' => $request->category_name,
+//            'user_id' => Auth::user()->id,
+//            'created_at' => Carbon::now()
+//        ]);
 
         //2nd insert method
 //        $category = new Category;
 //        $category->category_name = $request->category_name;
 //        $category->user_id = Auth::user()->id;
 //        $category->save();
+
+        //insert data using query builder
+        $data = array();
+        $data['category_name'] = $request->category_name;
+        $data['user_id'] = Auth::user()->id;
+        DB::table('categories')->insert($data);
+
         return Redirect()->back()->with('success', 'Category Inserted Successfully');
     }
 }
